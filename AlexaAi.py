bee2a5a7f1cfa4fea7f0364d9adaef379feebcba
ICC TEAM <131507368+GOVIND-BOTS@users.noreply.github.com>
2024-03-28 22:45:49 +0530

    Update AlexaAi.py

--- AlexaAi.py
+++ AlexaAi.py
@@ -170,5 +170,4 @@ async def alexastickerai(client: Client, message: Message):
 @client.on_message(
     (filters.text | filters.sticker)
     & filters.private
-    & ~filters.me
-)    
+    & ~filters.me) 
